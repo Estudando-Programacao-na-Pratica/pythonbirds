8c375b85ef699529c104ed3a572eb1476c036a70
olv-leo <81955817lL@>
2023-02-17 07:46:41 -0300

    Criado método cumprimentar e atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,1 +1,15 @@
-class ExemploPessoa:
+class Pessoa:
+    def __init__(self, nome=None, idade=None):
+        self.nome = nome
+        self.idade = idade
+
+    # método
+    def cumprimentar(self):
+        return f'Olá {self.nome}! Idade: {self.idade} Id: {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa('Leo', 26)
+    print(p.cumprimentar())
+    p.nome = 'Karine'
+    p.idade = 24
+    print(p.cumprimentar())
