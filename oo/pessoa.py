c96bd0c3c55db5133dd9c723b0bd6f8e83dfff53
olv-leo <81955817lL@>
2023-02-17 07:52:07 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,15 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=None):
+    def __init__(self, *filhos, nome=None, idade=None):
+        # atributos de instância
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     # método
     def cumprimentar(self):
         return f'Olá {self.nome}! Idade: {self.idade} Id: {id(self)}'
 
+
 if __name__ == '__main__':
-    p = Pessoa('Leo', 26)
+    p = Pessoa('Luiz', 'Luana', nome='Leo', idade=26)
+    p.filhos.append(Pessoa(nome='Lucas', idade=10))  # um objeto também pode ser adicionado na lista
     print(p.cumprimentar())
     p.nome = 'Karine'
     p.idade = 24
     print(p.cumprimentar())
+    print(p.filhos)
